baeb9ba3a80ba13af1d26e67080d5291dbc09ea0
Mihir Raj <118722608+Mihir200@users.noreply.github.com>
2024-04-10 09:18:53 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -13,7 +13,7 @@ driver.get("https://mihir200.github.io/webpage/")  # Update this with the path t
 time.sleep(2)  # Adding a delay to see the result
 
 # Assert some condition to verify the result
-assert "My First website" in driver.title
+assert "Forest" in driver.title
 
 # Take a screenshot
 timestamp = time.strftime("%Y%m%d-%H%M%S")
